c3bd88688cbfdb13fa7b930e0675db7264e0b439
高橋岳宏 <taka.mc.0509@gmail.com>
2017-07-03 17:31:06 +0900

    必要ファイルを作成＆接続

---
+++ SimpleTodoApps/EditViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EditViewController.swift
+//  SimpleTodoApps
+//
+//  Created by 高橋岳宏 on 2017/07/03.
+//  Copyright © 2017年 takehiro. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ SimpleTodoApps/SettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingViewController.swift
+//  SimpleTodoApps
+//
+//  Created by 高橋岳宏 on 2017/07/03.
+//  Copyright © 2017年 takehiro. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
