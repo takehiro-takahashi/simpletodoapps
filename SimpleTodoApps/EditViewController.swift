586258270064b39b6177bb9fac8691ade396d22d
高橋岳宏 <taka.mc.0509@gmail.com>
2017-07-03 18:47:45 +0900

    ・一覧画面で選択されたセルの行番号を受取 ・選択されたセルの行に値するToDoを取得し、textViewに表示

--- SimpleTodoApps/EditViewController.swift
+++ SimpleTodoApps/EditViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class EditViewController: UIViewController {
+class EditViewController: UIViewController, UITextViewDelegate {
+    
+    // 選択されたセルの番号を取得する変数
+    var selectedNumber:Int = 0
+    
+    var todoArray = [String]()
+    
+    // テキストを表示するエリア
+    @IBOutlet var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // todoArrayアプリ内から選択されたセルの行を取得する
+        if UserDefaults.standard.object(forKey: "todoArray") != nil {
+            todoArray = UserDefaults.standard.object(forKey: "todoArray") as! [String]
+            
+            // 前の画面で選択された番号の文字を取り出し、テキストビューに反映
+            textView.text = todoArray[selectedNumber]
+        }
+    }
+    
+    // テキストビュー以外の箇所をタッチされたらキーボードを閉じる
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if textView.isFirstResponder {
+            // キーボードを閉じる
+            textView.resignFirstResponder()
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
